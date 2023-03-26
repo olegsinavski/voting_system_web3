172300c5672ca70b0c4c979d9bfafb4da8f43113
Oleg Sinavski <sinavski@wayve.ai>
2023-03-26 18:31:36 +0100

    few things

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+// import { ethers } from 'ethers';
+// import { Contract } from 'ethers';
+// import contractABI from '../artifacts/contracts/VotingSystem.sol/VotingSystem.json';
+
+
+// const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
+// const contractAddress = '0x...'; // replace with your contract address
+// const contract = new Contract(contractAddress, contractABI.abi, provider);
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Hello, World!</h1>
+        </div>
       </header>
     </div>
   );
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,24 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  // paths: {
+  //   // ...
+  //   client: './frontend/build'
+  // },
+  // react: {
+  //   // Specify the directory where your React app will be built
+  //   // This will create a new build folder inside your client directory
+  //   buildDir: './frontend/build',
+  //   // Specify the location of your index.html file
+  //   html: {
+  //     template: './frontend/public/index.html'
+  //   },
+  //   // Specify the location of your index.js file
+  //   jsEntry: './frontend/index.js',
+  //   // Specify the location of your contracts
+  //   contracts: './contracts',
+  //   // Specify the name of your contract to use in your React components
+  //   // You will need to change this to the name of your own contract
+  //   contractName: 'VotingSystem'
+  // }
 };
