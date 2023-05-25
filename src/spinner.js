3f32515fbbe34e27b95e55408296b6fa64f03f3e
Oleg Sinavski <sinavski@wayve.ai>
2023-05-25 22:20:30 +0100

    fix spinner

--- src/spinner.js
+++ src/spinner.js
@@ -4,13 +4,13 @@
  */
 export default function Spinner({loading}) {
     return (
-        <div>
+        <>
             {loading && (
             <div className="loader_overlay">
                 <div className="loader_container">
                 <div className="loader"></div>
                 </div>
             </div>)}
-        </div>
+        </>
     );
 }
